9677d46ce6e910c1a21d6bbcc2ff78333e77e588
Dddarknight <9801677@gmail.com>
2022-09-02 14:35:03 +0300

    changes

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from urllib.error import HTTPError
 from page_loader.html_page import download
 from page_loader.cli import parse
 from page_loader.logger import setup_logging
@@ -9,7 +10,11 @@ import sys
 def main():
     logger = setup_logging()
     args = parse()
-    html_path = download(args.url, output=args.output)
+    try:
+        html_path = download(args.url, output=args.output)
+    except Exception as e:
+        logger.debug(f'An error occured during the execution of a program: {e}')
+        sys.exit(0)
     print(f'Page was downloaded as: {html_path}')
 
 
